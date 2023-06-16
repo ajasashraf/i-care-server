33f1a7e71f223ee05ec3bb6db9f64d2a58df1fd0
Ajas Ashraf <ajasashraf7010@gmail.com>
2023-06-16 11:41:10 +0530

    * changed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(logger("dev"));
 connection();
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://icaretech.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
     allowedHeaders: ["Content-type", "Access", "Authorization"],
